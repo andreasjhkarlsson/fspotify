50901a16f172ba8bf3f405267184deffd7bd56d4
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-06-21 21:15:14 +0200

    Implement user/profile endpoint

---
+++ src/User.fs
@@ -0,0 +1,31 @@
+﻿namespace FSpotify
+
+
+
+
+type SubscriptionLevel = |Premium |Free |Open
+
+type PrivateUser = {
+    birthdate: string option
+    country: Country option
+    display_name: string
+    email: string option
+    followers: Followers
+    href: Url
+    id: SpotifyId
+    images: Image list
+    product: SubscriptionLevel option
+    ``type``: string
+    uri: Url
+}
+
+module User =
+    
+    let me = 
+        Request.createFromEndpoint Request.Get "me"
+        |> Request.parse<PrivateUser,_>
+
+    let user (SpotifyId id) =
+        Request.createFromEndpoint Request.Get "users"
+        |> Request.withUrlPath id
+        |> Request.parse<PublicUser,_>
