60d6b307ca0f6f851724d119197eee5a94f221fc
Andreas Karlsson <andreas.karlsson3@gmail.com>
2015-07-03 12:37:21 +0200

    Add authentication persistence for example program

--- examples/Program.fs
+++ examples/Program.fs
@@ -1,4 +1,5 @@
 ﻿
+open System.IO
 open FSpotify
 open FSpotify.Authorization
 
@@ -10,11 +11,20 @@ let main argv =
     let clientSecret = "<secret>"
 
     let token =
-        [PlaylistReadPrivate; PlaylistReadCollaborative ;PlaylistModifyPublic
-         PlaylistModifyPrivate; Streaming; UserFollowModify; UserFollowRead
-         UserLibraryRead; UserLibraryModify; UserReadPrivate; UserReadBirthday
-         UserReadEmail]
-        |> UserAuthentication.authorize clientId clientSecret
+        try
+            let token = File.ReadAllText("token") |> Serializing.deserialize<Token>
+            User.me |> Request.withAuthorization token |> Request.send |> ignore
+            token
+        with
+        | _ ->
+            let token =
+                [PlaylistReadPrivate; PlaylistReadCollaborative ;PlaylistModifyPublic
+                 PlaylistModifyPrivate; Streaming; UserFollowModify; UserFollowRead
+                 UserLibraryRead; UserLibraryModify; UserReadPrivate; UserReadBirthday
+                 UserReadEmail]
+                |> UserAuthentication.authorize clientId clientSecret
+            token |> Serializing.serialize |> fun str -> File.WriteAllText("token",str)
+            token
 
     
     ArtistSummary.print (SpotifyId "2BpAc5eK7Rz5GAwSp9UYXa")
